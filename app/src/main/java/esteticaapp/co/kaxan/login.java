57ff9bbfe0ba6b9c6c5119c906b472bf1d1f94ab
Maximiliano <maximus.1626@gmal.com>
2018-05-30 19:39:41 -0500

    Cambio login (1.0)

--- app/src/main/java/esteticaapp/co/kaxan/login.java
+++ app/src/main/java/esteticaapp/co/kaxan/login.java
@@ -1,7 +1,10 @@
 package esteticaapp.co.kaxan;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button botonEntrar = (Button) findViewById(R.id.botonEntrar);
+
+        botonEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, menu.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
     }
+
+
 }
