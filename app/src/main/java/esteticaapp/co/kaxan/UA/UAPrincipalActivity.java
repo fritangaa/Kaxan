43fb22d904906151c7fb604e7bce0e3bc24686ac
ricardozal <ricardozalpich@gmail.com>
2018-11-11 02:12:43 -0600

    Muestra ubicacion del UA o no

--- app/src/main/java/esteticaapp/co/kaxan/UA/UAPrincipalActivity.java
+++ app/src/main/java/esteticaapp/co/kaxan/UA/UAPrincipalActivity.java
@@ -30,8 +30,11 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import esteticaapp.co.kaxan.R;
 import esteticaapp.co.kaxan.UM.menu;
@@ -41,10 +44,12 @@ public class UAPrincipalActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, MapaFragment.OnFragmentInteractionListener {
 
     DatabaseReference databaseReference;
+    DatabaseReference ref;
 
     private String lat ="";
     private String lon ="";
     private String bateria ="";
+    private String visible="";
 
     private Integer tmpEje = 5000;
 
@@ -74,6 +79,7 @@ public class UAPrincipalActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         databaseReference= FirebaseDatabase.getInstance().getReference("/ZxdtUxxfUoRrTw9dxoHA6XLAHqJ2").child("ubicacion");
+        ref = FirebaseDatabase.getInstance().getReference("/ZxdtUxxfUoRrTw9dxoHA6XLAHqJ2").child("configuracion").child("visible");
 
         handler.postDelayed(runnable, tmpEje);//empezamos a mandar datos
 
@@ -161,7 +167,7 @@ public class UAPrincipalActivity extends AppCompatActivity
         } else if (id == R.id.nav_premium) {
 
         } else if (id == R.id.nav_config) {
-            //startActivity(new Intent(UAPrincipalActivity.this, SettingsActivity.class));
+            startActivity(new Intent(UAPrincipalActivity.this, ConfigActivity.class));
         } else if (id == R.id.nav_help) {
 
         }
@@ -209,8 +215,29 @@ public class UAPrincipalActivity extends AppCompatActivity
         //--------------------------------------------------------------------------
 
         objUbicacion ubicacion = new objUbicacion(lat,lon,bateria);
+        objUbicacion no_visible = new objUbicacion("0","0","0");
 
-        databaseReference.setValue(ubicacion);
+
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                visible = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        if(visible.equals("1")){
+            databaseReference.setValue(ubicacion);
+        }else if(visible.equals("0")){
+            databaseReference.setValue(no_visible);
+        }
+
+
 
         return  true;
     }
